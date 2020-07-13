03d3c71d627cc522b278fa6675560067a66086a4
agdsarge <agdsargeant@gmail.com>
2020-07-13 14:50:41 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,19 @@
 export default function shoppingListItemReducer(
 	state = {
-		items: []
+		items: [] // default value of state
 	},
 	action
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length); // why do the old style interpolation?
+      		console.log('Updating state.items length to %s', state.items.length + 1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log('Initial state.items length: %s', state.items.length);
 			return state;
 	}
 }
